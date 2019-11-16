380846b857b8a972bbc34c9669d8acbe54ca5a5d
Jonas Dorfinger <jonas.dorfinger@gmx.at>
2019-11-16 21:57:47 +0100

    added function to print term overview, getShortMonthNames and add a Term to a tableCell

--- script/Month.js
+++ script/Month.js
@@ -39,8 +39,7 @@ class Month {
         let row = document.createElement('div');
         row.classList.add('row');
 
-        // for (let i = 0; i < previousMonth.getDate() + weekDays.length; i++) {
-        let currentDate = new Date(this.year, this.month, 0);
+        const currentDate = new Date(this.year, this.month, 0);
 
         for (let i = 0; i < currentDate.getDate() + weekDays.length + daysOfPrevoiusMonth; i++) {
             const weekDay = weekDays[i];
@@ -56,8 +55,19 @@ class Month {
                 dateInfoWrapper.appendChild(weekDayText);
             }
 
+            if (startDate.getDate() === new Date().getDate() && startDate.getFullYear() === new Date().getFullYear() && startDate.getMonth() === new Date().getMonth()) {
+                dateText.classList.add('currentDay');
+            }
+
+            if (startDate.getMonth() !== this.getDate().getMonth()) {
+                dateInfoWrapper.classList.add('lastOrNextMonth');
+            }
+
+            dateText.classList.add('dayOfMonth');
             dateText.textContent = startDate.getDate();
             dateInfoWrapper.appendChild(dateText);
+
+            tableCell.setAttribute('id', `${getShortMonthName(startDate.getMonth() + 1)}${startDate.getDate()}${startDate.getFullYear()}`);
 
             startDate = new Date(startDate.getFullYear(), startDate.getMonth(), startDate.getDate() + 1);
 
@@ -93,8 +103,140 @@ class Month {
     getDate() {
         return new Date(this.year, this.month, 0);
     }
+
+    addTerm(term) {
+        const startDate = term.startDate;
+        const startTime = term.startTime;
+        const subject = term.subject;
+        const allDay = term.allDay;
+        const tableCell = document.getElementById(`${getShortMonthName(startDate.getMonth() + 1)}${startDate.getDate()}${startDate.getFullYear()}`);       
+
+        const newTerm = document.createElement('div');
+
+        if (allDay) {
+            const subjectText = document.createElement('span');
+            subjectText.textContent = subject;
+
+            newTerm.classList.add('termAllDay');
+            newTerm.appendChild(subjectText);
+        } else {
+            const subjectText = document.createElement('strong');
+            subjectText.textContent = subject;
+
+            const circle = document.createElement('div');
+            circle.classList.add('termCircle');
+
+            const startTimeText = document.createElement('span');
+            startTimeText.textContent = startTime;
+
+            newTerm.classList.add('termTime');
+            newTerm.appendChild(circle);
+            newTerm.appendChild(startTimeText);
+            newTerm.appendChild(subjectText);
+        }
+        
+        tableCell.appendChild(newTerm);
+    }
+
+    printOverview(terms) {
+        const overview = document.querySelector('#overview');
+
+        // sort terms by timestamp
+        for (let i = 0; i < terms.length; i++) {
+            for (let j = i; j < terms.length; j++) {
+                if (terms[i].startDate.getTime() > terms[j].startDate.getTime()) {
+                    let temp = terms[i];
+                    terms[i] = terms[j];
+                    terms[j] = temp;
+                }
+            }
+        }
+
+        if (terms.length > 0) {
+            for (const term of terms) {
+                const newTerm = document.createElement('div');
+                newTerm.classList.add('overviewTerm');
+
+                const subject = document.createElement('p');
+                subject.textContent = term.subject;
+
+                subject.classList.add('overviewSubject');
+
+                newTerm.appendChild(subject);
+
+                const startDate = document.createElement('p');
+                startDate.textContent = term.startDate.toLocaleDateString();
+                newTerm.appendChild(startDate);
+
+                if (!term.allDay) {
+                    const time = document.createElement('p');
+                    time.textContent = `${term.startTime} - ${term.endTime} Uhr`;
+                    newTerm.appendChild(time);
+                } else {
+                    const time = document.createElement('p');
+                    time.textContent = `Ganztägig`;
+                    newTerm.appendChild(time);
+                }
+
+                overview.appendChild(newTerm);
+            }
+        } else {
+            const error = document.createElement('p');
+            error.textContent = 'Du hast in diesem Monat keine Termine.';
+            error.classList.add('error');
+
+            overview.appendChild(error);
+        }
+    }
+
+    removeOverviewTerms() {
+        let elements = document.querySelectorAll('#overview .overviewTerm');
+
+        if (elements.length > 0) {
+            for (const term of elements) {
+                document.querySelector('#overview').removeChild(term);
+            }
+        }
+
+        elements = document.querySelectorAll('#overview .error');
+
+        if (elements.length > 0) {
+            for (const term of elements) {
+                document.querySelector('#overview').removeChild(term);
+            }
+        }
+    }
 }
 
 function getWeekDay(date) {
     return date.getDay();
+}
+
+function getShortMonthName(monthNumber) {
+    switch (monthNumber) {
+        case 1:
+            return 'jan';
+        case 2:
+            return 'feb';
+        case 3:
+            return 'mar';
+        case 4:
+            return 'apr';
+        case 5:
+            return 'may';
+        case 6:
+            return 'jun';
+        case 7:
+            return 'jul';
+        case 8:
+            return 'aug';
+        case 9:
+            return 'sep';
+        case 10:
+            return 'oct';
+        case 11:
+            return 'nov';
+        case 12:
+            return 'dec';                                                                                                                                                                                                                                                                                                                                                                                                    
+    }
 }
