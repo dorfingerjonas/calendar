97c7470a52b1d6a85e77e4c9ffa595e2a43bfc49
Jonas Dorfinger <jonas.dorfinger@gmx.at>
2019-11-16 21:59:02 +0100

    converted csv input to workable array

--- php/calendar.php
+++ php/calendar.php
@@ -1,9 +1,22 @@
 <?php
 $file = fopen("../data2019.csv", "r") or die("Unable to open file!");
 $content = [];
+$terms = [];
 
 while (!feof($file)) {
     $content[] = fgets($file);
 }
 
-echo json_encode($content);
+for ($i = 1; $i < sizeof($content); $i++) {
+    $lineContent = explode(";", $content[$i]);
+    $line = [];
+    $line[] = $lineContent[0];
+    # $line[] = date("m/d/y H:i", strtotime($lineContent[1] . " " . $lineContent[2]));
+    # $line[] = date("m/d/y H:i", strtotime($lineContent[3] . " " . $lineContent[4]));
+    $line[] = $lineContent[1] . " " . $lineContent[2];
+    $line[] = $lineContent[3] . " " . $lineContent[4];
+    $line[] = strtolower($lineContent[5]);
+    $terms[] = $line;
+}
+
+echo json_encode($terms);
