fccb8034fa5719712e2a738952a9417da8e09055
Jonas Dorfinger <jonas.dorfinger@gmx.at>
2019-11-18 21:38:23 +0100

    added request handler for file write

--- php/calendar.php
+++ php/calendar.php
@@ -1,22 +1,35 @@
 <?php
-$file = fopen("../data2019.csv", "r") or die("Unable to open file!");
-$content = [];
-$terms = [];
+$req = $_POST['req'];
 
-while (!feof($file)) {
-    $content[] = fgets($file);
+if ($req === 'dataRequest') {
+    $file = fopen("../data2019.csv", "r") or die("Unable to open file!");
+    $content = [];
+    $terms = [];
+    
+    while (!feof($file)) {
+        $content[] = fgets($file);
+    }
+    
+    for ($i = 1; $i < sizeof($content); $i++) {
+        $lineContent = explode(";", $content[$i]);
+        $line = [];
+        $line[] = $lineContent[0];
+        # $line[] = date("m/d/y H:i", strtotime($lineContent[1] . " " . $lineContent[2]));
+        # $line[] = date("m/d/y H:i", strtotime($lineContent[3] . " " . $lineContent[4]));
+        $line[] = $lineContent[1] . " " . $lineContent[2];
+        $line[] = $lineContent[3] . " " . $lineContent[4];
+        $line[] = strtolower($lineContent[5]);
+        $terms[] = $line;
+    }
+    
+    echo json_encode($terms);
+} else {
+    $file = fopen("../data2019.csv", "a") or die("Unable to open file!");
+    fwrite($file, "\n" . $req);
+    fclose($file);
+
+    $response = [];
+    $response[] = 'success';
+
+    echo json_encode($response);
 }
-
-for ($i = 1; $i < sizeof($content); $i++) {
-    $lineContent = explode(";", $content[$i]);
-    $line = [];
-    $line[] = $lineContent[0];
-    # $line[] = date("m/d/y H:i", strtotime($lineContent[1] . " " . $lineContent[2]));
-    # $line[] = date("m/d/y H:i", strtotime($lineContent[3] . " " . $lineContent[4]));
-    $line[] = $lineContent[1] . " " . $lineContent[2];
-    $line[] = $lineContent[3] . " " . $lineContent[4];
-    $line[] = strtolower($lineContent[5]);
-    $terms[] = $line;
-}
-
-echo json_encode($terms);
